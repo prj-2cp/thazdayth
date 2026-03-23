c6f65d276388968af8567c0dafd86ffa658c6165
moussaoui1228 <moussaouisarah78@gmail.com>
2026-03-23 19:23:37 +0100

    cleaning the code

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,7 +4,6 @@ require("dotenv").config() ;
 
 //i used resend 
 const resend = new Resend(process.env.RESEND_API_KEY);
-console.log("before sending email") ;
 async function sendVerification(email, code) {
   const response = await resend.emails.send({
     from: "Thazdayth@resend.dev",
@@ -12,7 +11,6 @@ async function sendVerification(email, code) {
     subject: "Your Verification Code",
     html: `<p>Your code is: <strong>${code}</strong></p>`
   });
-  console.log("after sending email") ;
   console.log(response) ;
 }
 exports.sendVerification  = sendVerification ;
